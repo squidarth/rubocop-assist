7b0824e7cafccc2194f8935d5abb0206b0fb577b
Sidharth Shanker <sshanker220@gmail.com>
2017-10-23 23:30:19 -0700

    Updated the server code.

---
+++ webserver/runner.rb
@@ -0,0 +1,50 @@
+require 'rubocop'
+require 'active_support/all'
+require 'json'
+require 'timeout'
+require 'parser/current'
+
+stream = StringIO.new
+$stdout = stream
+
+def get_class_name(ast)
+  return nil unless ast.class == Parser::AST::Node
+  class_name = nil
+  ast.children.each do |child|
+    if child.respond_to?(:type) && child&.type == :class
+      class_name = child.children.first.children[1]
+    else
+      class_name = get_class_name(child)
+    end
+  end
+  class_name.to_s
+end
+
+def eval_rule(code, rule, rule_name)
+  Timeout::timeout(2) {
+    eval rule
+  }
+
+  options, = RuboCop::Options.new.parse(
+    ['--format', 'simple','--only', rule_name]
+  )
+  options[:stdin] = code
+  runner = RuboCop::Runner.new(options, RuboCop::ConfigStore.new)
+
+  Timeout::timeout(2) {
+    runner.run(['x.rb'])
+  }
+  output = $stdout.string
+
+  STDOUT.puts $stdout.string
+  $stdout.string = ''
+
+  output
+end
+
+code = ARGV[0].dup
+rule = ARGV[1].dup
+
+parsed_rule = Parser::CurrentRuby.parse(rule)
+rule_name = get_class_name(parsed_rule)
+puts eval_rule(code, rule, rule_name)
--- webserver/server.rb
+++ webserver/server.rb
@@ -5,63 +5,15 @@ require 'open3'
 
 set :bind, '0.0.0.0'
 
-def get_program
-  <<-RUBY
-  require 'rubocop'
-  require 'active_support/all'
-  require 'json'
-  require 'timeout'
-  require 'parser/current'
-
-  stream = StringIO.new
-  $stdout = stream
-
-  def get_class_name(ast)
-    return nil unless ast.class == Parser::AST::Node
-    class_name = nil
-    ast.children.each do |child|
-      if child.respond_to?(:type) && child&.type == :class
-        class_name = child.children.first.children[1]
-      else
-        class_name = get_class_name(child)
-      end
-    end
-    class_name.to_s
-  end
-
-  def eval_rule(code, rule, rule_name)
-    Timeout::timeout(2) {
-      eval rule
-    }
-
-    options, = RuboCop::Options.new.parse(
-      ['--format', 'simple','--only', rule_name]
-    )
-    options[:stdin] = code
-    runner = RuboCop::Runner.new(options, RuboCop::ConfigStore.new)
-
-    Timeout::timeout(2) {
-      runner.run(['x.rb'])
-    }
-    output = $stdout.string
-
-    STDOUT.puts $stdout.string
-    $stdout.string = ''
-
-    output
-  end
-
-  code = ARGV[0].dup
-  rule = ARGV[1].dup
-
-  parsed_rule = Parser::CurrentRuby.parse(rule)
-  rule_name = get_class_name(parsed_rule)
-  puts eval_rule(code, rule, rule_name)
-  RUBY
-end
-
 def execute_ruby(code, rule)
-  stdout, = Open3.capture2('ruby', '-e', get_program, code, rule, rlimit_cpu: [2,2])
+  stdout, = Open3.capture2(
+    'ruby',
+    'runner.rb',
+    code,
+    rule,
+    rlimit_cpu: [2, 2],
+    rlimit_nproc: 0
+  )
   stdout
 end
 
@@ -75,7 +27,7 @@ end
 
 post '/parse' do
   content_type :json
-  response = Parser::CurrentRuby.parse(params["code"]).to_s
+  response = Parser::CurrentRuby.parse(params['code']).to_s
   return { ast: response }.to_json
 end
 
